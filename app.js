e779c7834603854176547118edc9d884f6a37407
Ajinkya Ajay Pundle <ratnadeeppundle@gmail.com>
2025-01-13 19:55:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".about",{
+    strings:["ENTC Engineer|CDAC certified|Aspiring Web Developer|Game Tester"],
+            typeSpeed: 25,
+            loop:false,
+            showCursor: false
+})
